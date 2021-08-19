c5e7a4e3d243dca6982817b1a88b236c5ffa2863
JKLDain <88303710+JKLDain@users.noreply.github.com>
2021-08-19 14:38:52 +0900

    noise_reduce

--- __init__.py
+++ __init__.py
@@ -1,8 +1,1 @@
-__all__ = ["species", "CRN", "schemas"]
-
-from crn.reaction import *
-from crn.simulation import *
-import crn.utils as utils
-utils.stochpy_fix()
-from crn.crn import *
-
+from noisereduce.noisereduce import reduce_noise
